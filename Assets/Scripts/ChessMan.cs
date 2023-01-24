8601902e417173e96b9a31681489e61bee5711e5
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-01-25 01:26:27 +0530

    finish chesman code

--- Assets/Scripts/ChessMan.cs
+++ Assets/Scripts/ChessMan.cs
@@ -35,13 +35,27 @@ public class ChessMan : MonoBehaviour
             case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
             case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
 
-            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
-            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
-            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
-            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
-            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
-            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
         }
+    }
+
+    public void SetCoords()
+    {
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
     }
 
 }
