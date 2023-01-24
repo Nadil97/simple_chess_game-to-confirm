e454475c57f34e70e53794ccad97db65f7d1467f
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-01-25 00:54:19 +0530

    create switch case

--- Assets/Scripts/ChessMan.cs
+++ Assets/Scripts/ChessMan.cs
@@ -15,5 +15,33 @@ public class ChessMan : MonoBehaviour
     //Variable to Keep track of "black" player "white" Player
     private string player;
 
-    //references for
+    //references for all sprites that the chesspiese can be
+    public Sprite black_queen, black_knight, black_bishop, black_king, black_rook, black_pawn;
+    public Sprite white_queen, white_knight, white_bishop, white_king, white_rook, white_pawn;
+
+    public void Activate()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        //take the instantiated location and adjust the transform
+        SetCoords();
+
+        switch(this.name)
+        {
+            case "black_queen":this.GetComponent<SpriteRenderer>().sprite = black_queen;break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+        }
+    }
+
 }
